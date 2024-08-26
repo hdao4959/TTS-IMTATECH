47e10b1294a0d523dc24c4557222c54d91b9d9ff
lucnt09 <nguyentienluc1924@gmail.com>
2024-08-27 00:44:10 +0700

    CRUD CATEGORY

--- BE/routes/web.php
+++ BE/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
@@ -24,7 +25,8 @@ Route::prefix('admin')->as('admin.')->group(function() {
     Route::get('/', function() {
         return view('admin.dashboard');
     })->name('dashboard');
-    
+
+    Route::resource('categories', CategoryController::class);
     Route::resource('posts', PostController::class);
     Route::resource('users', UserController::class);
     Route::get('/user/{id}', [UserController::class, 'active']);
